4f1d4c029af2f725b8ced2ca2b3b4f3de7ca9ff6
jackfitzpatrick <jackfitzpatrick@wifi-roaming-128-4-82-171.host.udel.edu>
2017-02-27 02:50:22 -0500

    asdf

--- src/test/java/pkgEmpty/PS2_Tests.java
+++ src/test/java/pkgEmpty/PS2_Tests.java
@@ -3,39 +3,35 @@ package pkgEmpty;
 import static org.junit.Assert.*;
 
 import org.junit.After;
+
 import org.junit.AfterClass;
+
 import org.junit.Before;
+
 import org.junit.BeforeClass;
+
 import org.junit.Test;
+
+
 
 public class PS2_Tests {
 	
 	
-	int integerA = 1;
-	int integerB = 2;
-	int integerC = 3;
-	int integerD = 4;
-	int integerE = 5;
-	int integerF = 11;
-	int integerG = 12;
-	int integerH = 22;
-	
-	
-	MyInteger test1 = new MyInteger(integerA);
-	
-	MyInteger test2 = new MyInteger(integerB);
-	
-	MyInteger test3 = new MyInteger(integerC);
-	
-	MyInteger test4 = new MyInteger(integerD);
-	
-	MyInteger test5 = new MyInteger(integerE);
+	int integerA = 2;
+	int integerB = 3;
+	int integerC = 4;
+	int integerD = 5;
 
-	MyInteger test11 = new MyInteger(integerF);
 	
-	MyInteger test12 = new MyInteger(integerG);
 	
-	MyInteger test20 = new MyInteger(integerH);
+		MyInteger test1 = new MyInteger(integerA);
+	
+		MyInteger test2 = new MyInteger(integerB);
+	
+		MyInteger test3 = new MyInteger(integerC);
+	
+		MyInteger test4 = new MyInteger(integerD);
+		
 
 
 	@BeforeClass
@@ -57,44 +53,41 @@ public class PS2_Tests {
 	@Test
 	public void testisEven() {
 		
-		assertTrue(MyInteger.isEven(integerB));
-		assertFalse(MyInteger.isEven(integerA));
-		assertFalse(MyInteger.isEven(test1));
-		assertTrue(MyInteger.isEven(test2));
+		assertEquals(true, MyInteger.isEven(integerB));
+		assertEquals(false, MyInteger.isEven(integerA));
+		
+		assertEquals(false, MyInteger.isEven(test1));
+		assertEquals(true, MyInteger.isEven(test2));
 	}
 
 	@Test
 	public void testisOdd() {
 		
-		assertTrue(MyInteger.isOdd(integerC));
-		assertFalse(MyInteger.isOdd(integerD));
-		assertFalse(MyInteger.isOdd(test4));
-		assertTrue(MyInteger.isOdd(test3));
+		assertEquals(true, MyInteger.isOdd(integerC));
+		assertEquals(false, MyInteger.isOdd(integerD));
+		
+		assertEquals(false, MyInteger.isOdd(test4));
+		assertEquals(true, MyInteger.isOdd(test3));
 	}
 
 	@Test
 	public void testisPrime() {
 		
-		assertTrue(MyInteger.isPrime(integerE));
-		assertTrue(MyInteger.isPrime(integerF));
-		assertFalse(MyInteger.isPrime(integerG));
-		assertFalse(MyInteger.isPrime(integerH));
-		assertTrue(MyInteger.isPrime(test5));
-		assertTrue(MyInteger.isPrime(test11));
-		assertFalse(MyInteger.isPrime(test12));
-		assertFalse(MyInteger.isPrime(test20));
+		assertEquals(true, MyInteger.isPrime(integerD));
+		assertEquals(false, MyInteger.isPrime(integerA));
 		
-		boolean number = MyInteger.isPrime(test4);
-		assertEquals(test4.isPrime(),number);
+		assertEquals(true, MyInteger.isPrime(test2));
+		assertEquals(false, MyInteger.isPrime(test3));
 		
 	}
 
 	@Test
 	public void testequals() {
 		
-		assertTrue(test12.equals(integerG));
-		assertFalse(test12.equals(test5));
-		assertFalse(test12.equals(integerF));
-		assertEquals(test5.equals(test5),true);
+		assertEquals(true, test1.equals(integerA));
+		assertEquals(true, test2.equals(test2));
+		assertEquals(false, test3.equals(test1));
+		assertEquals(false, test4.equals(integerC));
+		
 	}
 }
